a03427cc6a47865e8c398acfcaca884bd34e64ce
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-02-03 16:32:15 -1000

    Update sqlInsert.php

--- sqlInsert.php
+++ sqlInsert.php
@@ -1,1 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "erizho21";
+$password = "zhou";
+$dbname = "erizho21";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Show Data
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql);
+
+$conn->close();
+?> 
