275731fa0a24d552e68127bfece4e1b60c9f0d8d
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-02-07 08:13:52 -1000

    Update sqlUpdate.php

--- sqlUpdate.php
+++ sqlUpdate.php
@@ -1,18 +1,19 @@
 <?php
 $servername = "localhost";
-$username = "erizho21";
+$username = "user";
 $password = "password";
 $dbname = "school";
 
-// Create connection
+//Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+//Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Update Data
+//Update Data
 $sql = "UPDATE students SET gradeLevel='12' WHERE name='Eric' AND gradeLevel=11";
 
+//Echos success or an error
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
 } else {
