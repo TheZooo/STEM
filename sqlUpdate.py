b505d72d39a1e09b8505443d0b5b96d966a443ba
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-02-07 08:12:08 -1000

    Update sqlUpdate.py

--- sqlUpdate.py
+++ sqlUpdate.py
@@ -1,10 +1,10 @@
 import MySQLdb
 #Connecting to database with mysqldb
-db = MySQLdb.connect(host="localhost", user="erizho21", passwd="password", db="school")
+db = MySQLdb.connect(host="localhost", user="user", passwd="password", db="school")
 #Creating cursor
 cur = db.cursor(MySQLdb.cursors.DictCursor)
-sql = "UPDATE students SET age='17' WHERE id=2"
-#Cursor executing sql code
+#Cursor executing sql code and commiting
+sql = "UPDATE students SET age='17' WHERE name='Eric' AND age=16"
 cur.execute(sql)
 db.commit()
 #Disconnecting cursor and database
