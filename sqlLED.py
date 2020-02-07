7e9b4a9eb4b6ecf3a709050f3378cc038dd104bd
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-02-07 09:20:38 -1000

    Update sqlLED.py

--- sqlLED.py
+++ sqlLED.py
@@ -2,36 +2,34 @@ import RPi.GPIO as GPIO
 import pymysql
 import time
 
-#setup the GPIO board and set the pin that we are using
+#Set GPIO pin 3 as an output
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 ledPin = 3
 GPIO.setup(ledPin, GPIO.OUT)
 
 def checker():
-	#connect to the server
+	#Connect to mysql on pi
 	db = pymysql.connect(host="localhost", user="piControl", passwd="f103", db="piLight")
 	cur = db.cursor(pymysql.cursors.DictCursor)
 	
-	#get the switchLight value
+	#Get the switchLight value from table lightState
 	sql = "SELECT switchLight FROM lightState"
 	cur.execute(sql)
 	row = cur.fetchall()
 	
-	#print the switchLight value
 	print(row[0]["switchLight"])
 	
-	#if switchLight is 0, turn the LED off
+	#Checks switchLight value and turns the LED on/off accordingly
 	if row[0]["switchLight"] is 0:
 		GPIO.output(ledPin, GPIO.LOW)
-	#if switchLight is 1, turn the LED on
 	elif row[0]["switchLight"] is 1:
 		GPIO.output(ledPin, GPIO.HIGH)
 		
 	cur.close()
 	db.close()
 	
-	#wait a bit, and then rerun checker()
+	#Wait half a second and loops
 	time.sleep(0.5)
 	checker()
 	
