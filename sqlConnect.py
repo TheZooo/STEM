7f890dc16b3dd4855173b68d81bffa6e7c0191d4
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-02-07 08:38:44 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -1,6 +1,6 @@
 import MySQLdb
 #Connecting to database with mysqldb
-db = MySQLdb.connect(host="localhost", user="erizho21", passwd="password", db="school")
+db = MySQLdb.connect(host="localhost", user="user", passwd="password", db="school")
 #Creating cursor
 cur = db.cursor(MySQLdb.cursors.DictCursor)
 sql = "SELECT * FROM students"
